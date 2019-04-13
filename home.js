a924a94bb3a9d9415e4f1a89c4f6c3434c131afa
Sam Murray <samvmurray@gmail.com>
2019-04-12 19:00:31 -0500

    Lat/Long fields are operational for updates. Buttons trigger a function, but no real functionality

--- home.js
+++ home.js
@@ -5,7 +5,8 @@ window.onload = function()
 	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	    maxZoom: 18,
+	    maxZoom: 16,
+	    minZoom: 9,
 	    id: 'mapbox.streets',
 	    accessToken: 'pk.eyJ1IjoibnNhdWVyIiwiYSI6ImNqdWNzY3hrazBkaDg0MHBhazVjcGE0cjkifQ.38ZAD_BPbe9s5ziTqG6U1A'
     }).addTo(mymap);
@@ -15,11 +16,13 @@ window.onload = function()
 	var mymap2 = L.map('mapid2').setView([51.505, -0.09], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	    maxZoom: 18,
+	    maxZoom: 16,
+	    minZoom: 9,
 	    id: 'mapbox.streets',
 	    accessToken: 'pk.eyJ1IjoibnNhdWVyIiwiYSI6ImNqdWNzY3hrazBkaDg0MHBhazVjcGE0cjkifQ.38ZAD_BPbe9s5ziTqG6U1A'
     }).addTo(mymap2);
     var marker = L.marker([51.5, -0.09]).addTo(mymap2);
+    
 
     var app = new Vue(
     {
@@ -28,12 +31,15 @@ window.onload = function()
         {
             latitude: "51.505",
             longitude: "-0.09",
+            latitude2: "51.505",
+            longitude2: "-0.09",
             coordinates: "44,-93",
             dateTo: "",
             dateFrom: "",
             radius: "",
             parameter: "",
             map: mymap,
+            map2: mymap2,
             orderBy: "location",
             //order_by, sort(desc), value_from, value_to
             url: "https://api.openaq.org/v1/measurements?limit=10&order_by=location",
@@ -42,26 +48,47 @@ window.onload = function()
         methods: {
             update: function(event){
                 console.log(event);
+                console.log(this.latitude + "&&" + this.longitude);
+                this.map.setView([this.latitude, this.longitude]);
             }
-        },
-        watch: {
-            latitude: function(val, oldVal) {
-                console.log(val + "&&" + oldVal);
-            }
-        },
+        }/*,
         mounted() 
         {
             axios
                 .get(this.url)
                 .then(response => (this.test = response))
-        }
+        }*/
     })
     console.log("vue created");
-    
-/*    function createUrl(appObj)
-    {
-        var final = appObj.url;
-        if (*/
+    mymap.on("moveend", ()=>(updateLatLong(app)))
+    mymap2.on("moveend", ()=>(updateLatLong2(app)))
+    document.getElementById("FSButton1").onclick = () => (makeFullScreen("FSButton1"));
+    document.getElementById("FSButton2").onclick = () => (makeFullScreen("FSButton2"));
 }
 
+function updateLatLong(app)
+{
+    var vals = app.map.getCenter()
+    app.latitude = vals.lat; 
+    app.longitude = vals.lng;
+}
+function updateLatLong2(app)
+{
+    var vals = app.map2.getCenter()
+    app.latitude2 = vals.lat; 
+    app.longitude2 = vals.lng;
+}
+function makeFullScreen(buttonId)
+{
+    var button = document.getElementById(buttonId);
+    console.log("button is working");
+    if (button.innerHTML.indexOf("Full") >= 0)
+    {
+        button.innerHTML = "Minimize";
+    }
+    else
+    {
+        button.innerHTML = "Make This Map Full-Screen";
+    }
+}
 
